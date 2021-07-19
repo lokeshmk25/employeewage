9ef9b133ff8d5d71f5909a596300ac08c839b0f6
lokeshmk25 <lokeshmk2211@gmail.com>
2021-07-19 10:21:38 +0530

    employee attendence

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,12 @@
 #! /bin/bash -x
-
-echo "hello world"
+#constant
+IS_PRESENT=1
+#variable
+empcheck=$((RANDOM%2))
+#selection
+if [ $empcheck -eq $IS_PRESENT ]
+then
+        echo "employee is present"
+else
+        echo "employee is absent"
+fi
