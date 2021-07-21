628449a3e7a490321c19ff818969c1b54e47f5fa
lokeshmk25 <lokeshmk2211@gmail.com>
2021-07-21 12:07:00 +0530

    added:part time

--- empwage.sh
+++ empwage.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -x
 #constant
-IS_PRESENT=1
+IS_PART_TIME=1
 WAGE_PER_HR=20
 #variable
 empcheck=$((RANDOM%2))
 #selection
-if [ $empcheck -eq $IS_PRESENT ]
+if [ $empcheck -eq $IS_PART_TIME ]
 then
         empHrs=8
         salary=$(( empHrs*WAGE_PER_HR ))
