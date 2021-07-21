ae874970d4a1ec88cbfb0db59a6d8e38aed0afd2
lokeshmk25 <lokeshmk2211@gmail.com>
2021-07-21 12:54:45 +0530

    UC-7-function

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,4 @@
+#! /bin/bash -x
 #constant
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
@@ -6,21 +7,27 @@ MAX_WORK_DAYS=20
 MAX_WORK_HR=100
 day=1
 totalworkHR=0
-
-while [ $day -le $MAX_WORK_DAYS ] && [ $totalworkHR -lt $MAX_WORK_HR ]
-do
-        #variable
-        empcheck=$(( RANDOM%3 ))
-
-        #selection
-        case $empcheck in
+function getworkHR(){
+        local empcheck=$1
+        local empHrs=0
+         case $empcheck in
                 $IS_PRESENT_FULL_TIME)
                         empHrs=8 ;;
                 $IS_PRESENT_HALF_TIME)
                         empHrs=4 ;;
         *)
-                empHrs=0 ;;
+                        empHrs=0;;
         esac
+        echo $empHrs
+}
+
+
+while [ $day -le $MAX_WORK_DAYS ] && [ $totalworkHR -lt $MAX_WORK_HR ]
+do
+        #variable
+        empcheck=$(( RANDOM%3 ))
+        ((day++))
+        empHrs="$( getworkHR $empcheck )"
         totalworkHR=$((totalworkHR + empHrs))
         salary=$(( empHrs*WAGE_PER_HR ))
 done
