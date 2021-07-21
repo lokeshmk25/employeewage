62459308f0cd891b4ba46906fee6c5e0515038cb
lokeshmk25 <lokeshmk2211@gmail.com>
2021-07-21 13:26:33 +0530

    added:totalwage

--- empwage.sh
+++ empwage.sh
@@ -1,15 +1,18 @@
 #! /bin/bash -x
 #constant
-IS_PRESENT_FULL_TIME=1
-IS_PRESENT_HALF_TIME=2
-WAGE_PER_HR=20
-MAX_WORK_DAYS=20
-MAX_WORK_HR=100
-day=1
-totalworkHR=0
+IS_PRESENT_FULL_TIME=2;
+IS_PRESENT_HALF_TIME=1;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+MAX_WORK_HR_IN_MONTH=4;
+
+#VARIBLES
+
+totalworkingDays=0;
+totalEmpHr=0;
+
 function getworkHR(){
         local empcheck=$1
-        local empHrs=0
          case $empcheck in
                 $IS_PRESENT_FULL_TIME)
                         empHrs=8 ;;
@@ -21,16 +24,20 @@ function getworkHR(){
         echo $empHrs
 }
 
+function getEmpWage() {
+	local $empHr=$1
+	echo (($empHr*EMP_RATE_PER_HR))
+}
 
-while [ $day -le $MAX_WORK_DAYS ] && [ $totalworkHR -lt $MAX_WORK_HR ]
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH ] && [ $totalworkingDays -lt $NUM_WORKING_DAYS ]]
 do
         #variable
         empcheck=$(( RANDOM%3 ))
-        ((day++))
-        empHrs="$( getworkHR $empcheck )"
-        totalworkHR=$((totalworkHR + empHrs))
-        salary=$(( empHrs*WAGE_PER_HR ))
+        ((totalWorkingDays++))
+        empHrs="$( getworkHrs $empcheck )"
+        totalEmpHrs=$((totalEmpHrs+$empHrs))
+	dailyWage[$totalWorkingDays]="$( getEmpwage $empHrs )"
 done
-totalsalary=$(( totalworkHR * WAGE_PER_HR ))
-
+totalsalary=$(( $totalEmpHrs*EMP_RATE_PER_HR ));
+echo ${dailyWage[@]}
 
