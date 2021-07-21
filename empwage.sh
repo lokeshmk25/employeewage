620634e9aff9ceda711743fb46063e56ba997f1e
lokeshmk25 <lokeshmk2211@gmail.com>
2021-07-21 12:21:30 +0530

    added:wagespermonth

--- empwage.sh
+++ empwage.sh
@@ -3,16 +3,24 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
 WAGE_PER_HR=20
-#variable
-empcheck=$(( RANDOM%3 ))
-#selection
-case $empcheck in
+MAX_WORK_DAYS=20
+totalworkHR=0
+
+for ((day=1;day<=$MAX_WORK_DAYS;day++))
+do
+        #variable
+        empcheck=$(( RANDOM%3 ))
+
+        #selection
+        case $empcheck in
          $IS_PRESENT_FULL_TIME)
                 empHrs=8 ;;
         $IS_PRESENT_HALF_TIME)
                 empHrs=4 ;;
         *)
                 empHrs=0 ;;
-esac
- salary=$(( empHrs*WAGE_PER_HR ))
-
+        esac
+        totalworkHR=$((totalworkHR + empHrs))
+        salary=$(( empHrs*WAGE_PER_HR ))
+done
+totalsalary=$(( totalworkHR * WAGE_PER_HR ))
